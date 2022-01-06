841c8f6bcefcd1c9b0b04fb92c5ef2fff0bd834d
whiteneofetch <rumordek@gmail.com>
2022-01-06 16:19:03 +0300

    +babylon

---
+++ client/pages/baby/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <Scene>
+    <babylon
+      model="https://models.babylonjs.com/boombox.glb"
+      templates.main.params.fill-screen="true"
+    ></babylon>
+  </Scene>
+</template>
---
+++ client/pages/baby/scene.vue
@@ -0,0 +1,15 @@
+<template>
+  <Scene>
+    <Box :position="[0, 0, 5]"></Box>
+  </Scene>
+</template>
+
+//
+<script>
+// import { Entity, Scene, Box } from 'vue-babylonjs'
+// export default {
+//   mixins: [Entity],
+//   components: { Scene, Box }
+// }
+//
+</script>
